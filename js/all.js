084508baece703f9213db9d58e7a415c0e16128a
yunhsi <cles30102@gmail.com>
2021-02-17 19:47:24 +0800

    fix

--- js/all.js
+++ js/all.js
@@ -1,5 +1,25 @@
 AOS.init();
 
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 200) {
+          $('.go_top').fadeIn();
+      } else {
+          $('.go_top').fadeOut();
+      }
+  }); 
+
+  $('.go_top').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 850);
+      return false;
+  });
+
+  $('.carousel').carousel({
+    interval: 3000,
+    pause:false
+  })
+});
+
 function checkScroll(){
     var startY = $('.navbar').height() * 2;
 
@@ -15,21 +35,6 @@ if($('.navbar').length > 0){
         checkScroll();
     });
 }
-
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 200) {
-            $('.go_top').fadeIn();
-        } else {
-            $('.go_top').fadeOut();
-        }
-    }); 
-
-    $('.go_top').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 850);
-        return false;
-    });
-});
 
 var swiper = new Swiper('.swiper-container', {
   pagination: {
@@ -54,8 +59,3 @@ var swiper = new Swiper('.swiper-container', {
     },
   }
 });
-
-$('.carousel').carousel({
-  interval: 3000,
-  pause:false
-})
